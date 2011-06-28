60a1a2f5528c3e193915312cf3de20b4d222cdb5
Vincent Bumgarner <vbumgarner@splunk.com>
2011-06-28 17:19:49 -0500

    Renamed to example/query* and run.sh

--- app/bin/example.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-QUERY_NAME=example
-cd `dirname $0`
-java -cp .:lib/* com.splunk.jdbcscriptedinput.Main $QUERY_NAME.properties $QUERY_NAME.pointer 2> $SPLUNK_HOME/var/log/splunk/jdbcscriptedinput.$QUERY_NAME.log
-#edit the classpath as needed
