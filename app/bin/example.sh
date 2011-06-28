bf479f5e9cc326952175c53ceca0d7da065d48e3
Vincent Bumgarner <vbumgarner@splunk.com>
2011-06-28 16:30:09 -0500

    added QUERY_NAME variable

--- app/bin/example.sh
+++ app/bin/example.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-java -cp .:lib/* com.splunk.jdbcscriptedinput.Main example.properties example.pointer 2> $SPLUNK_HOME/var/log/splunk/jdbcscriptedinput.example.log
+QUERY_NAME=example
+cd `dirname $0`
+java -cp .:lib/* com.splunk.jdbcscriptedinput.Main $QUERY_NAME.properties $QUERY_NAME.pointer 2> $SPLUNK_HOME/var/log/splunk/jdbcscriptedinput.$QUERY_NAME.log
 #edit the classpath as needed
