17c6f42fcd760e2811d765043cfd7e64d2693b1e
Vincent Bumgarner <vbumgarner@splunk.com>
2011-06-29 00:18:51 -0500

    Add log4j.xml. Also rotate the log so we don't run out of disk space.

--- app/bin/run.sh
+++ app/bin/run.sh
@@ -7,5 +7,12 @@ if test "$QUERY_NAME" == "" ; then
         exit
 fi
 
-java -cp .:lib/* com.splunk.jdbcscriptedinput.Main $QUERY_NAME/query.properties $QUERY_NAME/query.pointer 2> $SPLUNK_HOME/var/log/splunk/jdbcscriptedinput.$QUERY_NAME.log
+LOG_PATH=$SPLUNK_HOME/var/log/splunk/jdbcscriptedinput.$QUERY_NAME.log
+
+mv $LOG_PATH.4 $LOG_PATH.5
+mv $LOG_PATH.3 $LOG_PATH.4
+mv $LOG_PATH.2 $LOG_PATH.3
+mv $LOG_PATH.1 $LOG_PATH.2
+mv $LOG_PATH $LOG_PATH.1
+java -cp .:lib/* -Dlog4j.configuration=lib/log4j.xml com.splunk.jdbcscriptedinput.Main $QUERY_NAME/query.properties $QUERY_NAME/query.pointer 2> $LOG_PATH
 #edit the classpath as needed
