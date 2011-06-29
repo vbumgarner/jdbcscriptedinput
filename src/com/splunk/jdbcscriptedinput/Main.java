4a15680cbdaed936f4a954d1588ad880b4e74d9e
Vincent Bumgarner <vbumgarner@splunk.com>
2011-06-29 00:16:45 -0500

    Modified whitespace, apparently.

--- src/com/splunk/jdbcscriptedinput/Main.java
+++ src/com/splunk/jdbcscriptedinput/Main.java
@@ -5,9 +5,9 @@ import java.io.File;
 import org.apache.log4j.Logger;
 
 public class Main {
-	   private static final Logger logger = Logger.getLogger(Main.class);
+	private static final Logger logger = Logger.getLogger(Main.class);
 
-	   public static void main(String[] args) {
+	public static void main(String[] args) {
 		try {
 			if (args.length < 2) {
 				logger.error("Usage: java com.splunk.jdbcscriptedinput.Main queryType.properties queryType.pointer");
@@ -18,8 +18,8 @@ public class Main {
 			File configFile = new File(args[0]);
 			if (!configFile.exists()) {
 				logger.error("Properties file "
-								+ configFile.getAbsolutePath()
-								+ " doesn't exist. See the README and create an appropriate config.");
+						+ configFile.getAbsolutePath()
+						+ " doesn't exist. See the README and create an appropriate config.");
 				System.exit(-1);
 			}
 			Config config = Config.fromPropertiesFile(configFile);
@@ -28,8 +28,8 @@ public class Main {
 			File pointerFile = new File(args[1]);
 			if (!pointerFile.exists()) {
 				logger.error("Pointer file "
-								+ pointerFile.getAbsolutePath()
-								+ " doesn't exist. If this is the first time running, simply create an empty file and zero will be used as the starting point for your iteratorField. See the README for more info.");
+						+ pointerFile.getAbsolutePath()
+						+ " doesn't exist. If this is the first time running, simply create an empty file and zero will be used as the starting point for your iteratorField. See the README for more info.");
 				System.exit(-1);
 			}
 			Pointer pointer = new Pointer(pointerFile);
