4254c41d00ec26df85600bb4c203dfd5715143ba
Vincent Bumgarner <vbumgarner@splunk.com>
2011-06-28 20:33:07 -0500

    Added check for an argument.

--- app/bin/run.sh
+++ app/bin/run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 cd `dirname $0`
 QUERY_NAME=$1
+
+if test "$QUERY_NAME" == "" ; then
+        echo We need one argument, which is the name of a directory in bin, which contains two files. See README.txt.
+        exit
+fi
+
 java -cp .:lib/* com.splunk.jdbcscriptedinput.Main $QUERY_NAME/query.properties $QUERY_NAME/query.pointer 2> $SPLUNK_HOME/var/log/splunk/jdbcscriptedinput.$QUERY_NAME.log
 #edit the classpath as needed
